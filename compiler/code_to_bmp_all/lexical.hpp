779294f5345eb554b0db7751d42c36a0fd8ef4eb
A01236054 <a01236054@itesm.mx>
2024-05-23 17:38:55 -0600

    Added code to hex convertion

--- compiler/code_to_bmp_all/lexical.hpp
+++ compiler/code_to_bmp_all/lexical.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
@@ -8,29 +10,48 @@
 #include <vector>
 #include <cassert>
 
-#include "token.hpp"
-#include "tokenHex.hpp"
+#include "token_to_bmp.hpp"
 
 struct Lexical {
-    std::vector<Token> tokens;
+    std::vector<std::vector<std::string>> hex;
+    std::vector<Token> toBmp;
     std::map<std::string, std::pair<std::string, token::Type>> keyword;
+    
+    void pushHex(std::string color, int& numLine) {
+        while (hex.size() < numLine) {
+            hex.push_back({});
+        }
 
-    void reset() {
-        tokens.clear();
-        keyword.clear();
+        hex.back().push_back(color);
     }
 
-    void throwInvChar(int line, int column, std::string msg, std::string str) {
-        std::string lineMsg = " at line: " + std::to_string(line) + ",";
-        std::string colMsg = " column: " + std::to_string(column) + ".";
-        std::string strMsg = " Received: " + str;
-        throw std::invalid_argument(msg + lineMsg + colMsg + strMsg);
+    std::string charToHex(char c) {
+        std::stringstream ss;
+        ss << std::uppercase << std::hex;
+        ss << std::setw(2) << std::setfill('0') << std::left << (int) c * 2;
+
+        return ss.str() + std::string(4, '0');
+    }
+
+    void throwInvChar(int line, int column, std::string msg) {
+        std::string lineMsg = " at line: " + std::to_string(line) + ',';
+        std::string colMsg = " column: " + std::to_string(column);
+        throw std::invalid_argument(msg + lineMsg + colMsg);
     }
 
     void pushToken(std::string &str, int &s, int line, int col, token::Type type) {
         assert((int) str.size() == 1);
+
+        std::string color;
+        if (keyword.count(str)) {
+            color = keyword[str].first;
+        }
+        else {
+            color = charToHex(str[0]);
+        }
         
-        tokens.push_back(Token(type, str, line, col));
+        toBmp.push_back(Token(type, str, color, line, col));
+        pushHex(color, line);
 
         s = 0;
         str.clear();
@@ -38,7 +59,19 @@ struct Lexical {
 
     void pushTokenData(std::string &str, int &s, int line, int col, token::DataType dt) {
         int lenStr = str.size();
-        tokens.push_back(Token(dt, str, line, col - lenStr + 1));
+        for (int i = 0; i < lenStr; i++) {
+            std::string c = std::string(1, str[i]);
+            std::string color;
+            if (keyword.count(c)) {
+                color = keyword[c].first;
+            }
+            else {
+                color = charToHex(str[i]);
+            }
+
+            toBmp.push_back(Token(dt, c, color, line, col - lenStr + i));
+            pushHex(color, line);
+        }
 
         s = 0;
         str.clear();
@@ -65,8 +98,11 @@ struct Lexical {
                 else if (c == '\'') {
                     s = 5;
                 }
-                else {
+                else if (std::isalpha(c) || keyword.count(std::string(1, c))) {
                     s = 1;
+                }
+                else {
+                    throwInvChar(numLine, col, "Invalid character");
                 }
 
                 if (s > 0) {
@@ -79,21 +115,15 @@ struct Lexical {
                     if (col != lenLine - 1 && keyword.count(str + std::string(1, nxt))) {
                         continue;
                     }   
-                    if (col != lenLine - 1 && std::isalpha(str[0]) && std::isalnum(nxt)) {
-                        continue;
-                    }
                     
                     if (keyword.count(str)) {
                         auto &[color, type] = keyword[str];
                         int pos = col - lenStr + 1;
-                        tokens.push_back(Token(type, str, numLine, pos));
-
-                        if (type == token::Type::DATA) {
-                            tokens.back().dataType = token::DataType::BOOL;
-                        }
+                        toBmp.push_back(Token(type, str, color, numLine, pos));
+                        pushHex(color, numLine);
 
                         if (type == token::DATATYPE) {
-                            token::DataType& dt = tokens.back().dataType;
+                            token::DataType& dt = toBmp.back().dataType;
                             if (str == "number") {
                                 dt = token::DataType::NUMBER;
                             }
@@ -109,20 +139,14 @@ struct Lexical {
                         }
                         
                         if (str == "array") {
-                            tokens.back().subType = token::SubType::ARRAY;
+                            toBmp.back().subType = token::SubType::ARRAY;
                         }
                         else if (str == "func" || str == "function") {
-                            tokens.back().subType = token::SubType::FUNCTION;
+                            toBmp.back().subType = token::SubType::FUNCTION;
                         }
                     }
                     else {
-                        if (!std::isalpha(str[0])) {
-                            std::string msg = "Invalid character";
-                            throwInvChar(numLine, col, msg, std::string(str[0], 1));
-                        }
-
-                        token::Type type = token::Type::IDENTIFIER;
-                        tokens.push_back(Token(type, str, numLine, col - lenStr + 1));
+                        pushTokenData(str, s, numLine, col, token::DataType::UNDEFINEDDT);
                     }
                     
                     s = 0;
@@ -131,15 +155,15 @@ struct Lexical {
                 }
             }
             else if (s == 2 || s == 3) { // integer or double = number
-                if (col != lenLine - 1 && nxt == '.') {
+                if (c == '.') {
                     if (s == 3) {
-                        throwInvChar(numLine, col, "invalid character", str);
+                        throwInvChar(numLine, col, "invalid character");
                     }
 
                     s = 3;
                 }
                 else if (col == lenLine - 1 || !std::isdigit(nxt)) {
-                    pushTokenData(str, s, numLine, col, token::DataType::NUMBER);
+                    pushTokenData(str, s, numLine, col, token::NUMBER);
                 }
             }
             else if (s == 4) { // string
@@ -149,7 +173,7 @@ struct Lexical {
                     str += c;
                 }
                 else if (c == '"' && lenStr != 1) {
-                    pushTokenData(str, s, numLine, col, token::DataType::STRING);
+                    pushTokenData(str, s, numLine, col, token::STRING);
                 }
             }
             else if (s == 5) { // char
@@ -157,97 +181,91 @@ struct Lexical {
                     char e = str[std::min(2, lenStr - 1)];
                     bool isEsc = e == 'n' || e == 't' || e == 'r' || e == 'v' || e == 'f';
                     if (lenStr > 4 || lenStr == 2 || (lenStr == 4 && !isEsc)) {
-                        throwInvChar(numLine, col, "invalid char", str);
+                        throwInvChar(numLine, col, "invalid char");
                     }
 
-                    pushTokenData(str, s, numLine, col, token::DataType::CHAR);
+                    pushTokenData(str, s, numLine, col, token::CHAR);
                 }
             }
         }
     }
 
-    std::vector<Token>& tokenize(std::vector<std::vector<std::string>> hex) {
-        reset();
-        
-        std::map<std::string, std::pair<std::string, token::Type>> keywordHex;
-        for (int i = 0; i < tokenH::keywordsToCode.size(); i++) {
-            std::map<std::string, std::string>& m = tokenH::keywordsToCode[i].mapToCode;
-            for (const auto &[color, key] : m) {
-                keywordHex[color] = std::make_pair(key, tokenH::keywordsToCode[i].type);
-            }
-        }
-
+    void tokenize(std::string path) {
         for (int i = 0; i < token::keywordsToHex.size(); i++) {
-            std::map<std::string, std::string>& m = token::keywordsToHex[i].mapToHex;
+            std::map<std::string, std::string> &m = token::keywordsToHex[i].mapToHex;
             for (const auto &[key, color] : m) {
                 keyword[key] = std::make_pair(color, token::keywordsToHex[i].type);
             }
         }
 
+        std::ifstream file(path);
+        std::string line;
         int numLine = 0;
 
         int state = 0;
-        std::vector<std::string> lines, lineHexs;
-        std::stringstream ss;
-        for (int i = 0; i < (int) hex.size(); ++i) {
-            numLine += 1;
-            std::string line = "";
-            std::string lineHex = "";
-
-            for (const std::string& h : hex[i]) {
-                lineHex += h + " ";
-                if (!keywordHex.count(h)) {
-                    std::string firstTwo = h.substr(0, 2);
-                    std::string lastFour = h.substr(2, 4);
-                    ss << std::hex << firstTwo;
-                    
-                    int num;
-                    ss >> num;
-                    ss.clear();
-
-                    if (lastFour == "0000" && num >= 64 && num <= 252) {
-                        line += (char) (num / 2);
+        while (std::getline(file, line)) {          
+            if (line.find('\t') != std::string::npos) {
+                std::string newLine = "";
+                for (int i = 0; i < (int) line.size(); ++i) {
+                    char c = line[i];
+                    if (c == '\t') {
+                        newLine += "    ";
                     }
                     else {
-                        line += h;
+                        newLine += c;
                     }
                 }
-                else {
-                    line += keywordHex[h].first;
-                }
+
+                line = newLine;
             }
 
+            numLine += 1;
             std::cout << numLine << ' ' << line << '\n';
-            std::cout << numLine << ' ' << lineHex << '\n' << '\n';
-            lines.push_back(std::to_string(numLine) + " " + line);
-            lineHexs.push_back(std::to_string(numLine) + " " + lineHex);
             
             tokenizeLine(line, numLine, state);
         }
 
+        file.close();
+
+        if (state != 0) {
+            throwInvChar(numLine, line.size(), "Expected closing");
+        }
+
         std::cout << '\n';
-        int maxSize = std::max_element(tokens.begin(), tokens.end(), [&](Token &a, Token &b) {
+        int maxSize = std::max_element(toBmp.begin(), toBmp.end(), [&](Token &a, Token &b) {
             return a.value.size() < b.value.size();
         }) -> value.size();
 
+        int maxColor = std::max_element(toBmp.begin(), toBmp.end(), [&](Token &a, Token &b) {
+            return a.color.size() < b.color.size();
+        }) -> color.size();
         
-        int maxType = token::typeToStr[std::max_element(tokens.begin(), tokens.end(), [&](Token &a, Token &b) {
+        int maxType = token::typeToStr[std::max_element(toBmp.begin(), toBmp.end(), [&](Token &a, Token &b) {
             return token::typeToStr[a.type].size() < token::typeToStr[b.type].size();
         }) -> type].size();
 
-        for (auto &token : tokens) {
+
+        for (auto& token : toBmp) {
             std::cout << std::setw(maxSize) << std::right <<  token.value << ' ';
+            std::cout << std::setw(maxColor) << std::left << token.color << ' ';
             std::cout << std::setw(maxType) << std::left << token::typeToStr[token.type] << ' ';
             std::cout << token.line << ' ' << token.column << ' ' << '\n';
         }
 
         std::cout << '\n';
 
-        for (auto &line : lines) {
-            std::cout << line << '\n';
+        for (auto& token : toBmp) {
+            std::cout << token.color << ' ';
         }
-        std::cout << '\n';
-        
-        return tokens;
+
+        std::cout << '\n' << '\n' << '\n';
+
+        for (auto& h : hex) {
+            for (auto& c : h) {
+                std::cout << c << ' ';
+            }
+
+            std::cout << '\n';
+        }
     }
 };
---
+++ compiler/code_to_bmp_all/token_to_bmp.hpp
@@ -0,0 +1,245 @@
+#include <map>
+#include <string>
+#include <vector>
+
+namespace token {
+    enum Type {
+        UNDEFINEDT = 0,
+        ARITHMETIC,
+        ARRAYOP,
+        ASSIGNMENT,
+        CONDITIONAL,
+        COMPARISON,
+        DATA,
+        DATATYPE,
+        DELIMITATION,
+        IDENTIFIER,
+        INPUT,
+        LOGICAL,
+        LOOP,
+        OP,
+        OUTPUT,
+        RETURN,
+        SUBTYPE,
+        VOID,
+        WHITESPACE
+    };
+
+    std::map<Type, std::string> typeToStr = {
+        {ARITHMETIC, "ARITHMETIC"},
+        {ARRAYOP, "ARRAYOP"},
+        {ASSIGNMENT, "ASSIGNMENT"},
+        {CONDITIONAL, "CONDITIONAL"},
+        {COMPARISON, "COMPARISON"},
+        {DATA, "DATA"},
+        {DATATYPE, "DATATYPE"},
+        {DELIMITATION, "DELIMITATION"},
+        {LOGICAL, "LOGICAL"},
+        {LOOP, "LOOP"},
+        {IDENTIFIER, "IDENTIFIER"},
+        {INPUT, "INPUT"},
+        {OP, "OP"},
+        {OUTPUT, "OUTPUT"},
+        {RETURN, "RETURN"},
+        {SUBTYPE, "SUBTYPE"},
+        {VOID, "VOID"},
+        {WHITESPACE, "WHITESPACE"}
+    };
+
+    enum SubType {
+        UNDEFINEDST = 100,
+        ARRAY,
+        FUNCTION
+    };
+
+    std::map<SubType, std::string> subTypeToStr = {
+        {ARRAY, "ARRAY"},
+        {FUNCTION, "FUNCTION"}
+    };
+
+    enum DataType {
+        UNDEFINEDDT = 200,
+        BOOL,
+        CHAR,
+        STRING,
+        NUMBER
+    };
+
+    std::map<DataType, std::string> dataTypeToStr = {
+        {BOOL, "BOOL"},
+        {CHAR, "CHAR"},
+        {STRING, "STRING"},
+        {NUMBER, "NUMBER"},
+        {UNDEFINEDDT, "UNDEFINED"}
+    };
+
+    struct MapToHex {
+        std::map<std::string, std::string> mapToHex;
+        Type type;
+
+        MapToHex(std::map<std::string, std::string> mapToHex, Type type) : 
+            mapToHex(mapToHex), 
+            type(type) 
+            {}
+    };
+
+    std::map<std::string, std::string> arithmetic = {
+        {"+", "FFAADD"},
+        {"-", "FFAAAA"},
+        {"*", "FFAA77"},
+        {"/", "FFAA44"},
+        {"%", "FF8811"}
+    };
+
+    std::map<std::string, std::string> assignment = {
+        {"=", "FF5511"}
+    };
+
+    std::map<std::string, std::string> boolean = {
+        {"true", "50C878"},
+        {"false", "F62217"}
+    };
+
+    std::map<std::string, std::string> conditional = {
+        {"if", "33FF77"},
+        {"else", "33FF44"}
+    };
+
+    std::map<std::string, std::string> comparison = {
+        {"==", "AAFFFF"},
+        {"!=", "AACCCC"},
+        {"<", "AA1111"},
+        {">", "AA5555"},
+        {"<=", "FA1111"},
+        {">=", "FA5555"}
+    };
+
+    std::map<std::string, std::string> dataType = {
+        {"char", "FF0000"},
+        {"number", "0000FF"},
+        {"bool", "00FF00"},
+        {"array", "00FFFF"},
+        {"string", "FFFF00"},
+        {"func", "FF00FF"},
+        {"function", "FF00FF"}
+    };
+
+    std::map<std::string, std::string> delimitation = {
+        {"(", "2BB2FF"},
+        {")", "FF7B2B"},
+        {"|", "40CCB0"},
+        {"~", "4AEEB3"},
+        {"\"", "FFD37C"},
+        {";", "FF5353"}
+    };
+
+    std::map<std::string, std::string> isReturn = {
+        {"return", "BC7FFF"}
+    };
+
+    std::map<std::string, std::string> input = {
+        {"read", "6A09B0"},
+    };
+
+    std::map<std::string, std::string> isVoid = {
+        {"$", "000000"}
+    };
+    
+    std::map<std::string, std::string> loop = {
+        {"for", "33FFDD"},
+        {"while", "33FFAA"}
+    };
+    
+    std::map<std::string, std::string> logical = {
+        {"&&", "CC33FF"},
+        {"||", "FFFF55"},
+        {"!", "FFD0D0"},
+        {"==", "AAFFFF"},
+        {"!=", "AACCCC"},
+        {"<", "AA1111"},
+        {">", "AA5555"}
+    };
+
+    std::map<std::string, std::string> number = {
+        {"0", "0000FE"},
+        {"1", "0000EE"},
+        {"2", "0000DD"},
+        {"3", "0000CC"},
+        {"4", "0000BB"},
+        {"5", "0000AA"},
+        {"6", "000099"},
+        {"7", "000088"},
+        {"8", "000077"},
+        {"9", "000066"},
+        {".", "000033"}
+    };
+
+    std::map<std::string, std::string> output = {
+        {"print", "2024AB"}
+    };
+
+    std::map<std::string, std::string> op = {
+        {"_", "ABCD0E"}
+    };
+
+    std::map<std::string, std::string> subTypes = {
+        {"array", "00FFFF"},
+        {"func", "FF00FF"},
+        {"function", "FF00FF"}
+    };
+
+    std::map<std::string, std::string> whitespace = {
+        {" ", "FFFFFF"}
+    };
+
+    std::vector<MapToHex> keywordsToHex = {
+        MapToHex(arithmetic, ARITHMETIC),
+        MapToHex(assignment, ASSIGNMENT),
+        MapToHex(boolean, DATA),
+        MapToHex(conditional, CONDITIONAL),
+        MapToHex(comparison, COMPARISON),
+        MapToHex(dataType, DATATYPE),
+        MapToHex(delimitation, DELIMITATION),
+        MapToHex(input, INPUT),
+        MapToHex(isReturn, RETURN),
+        MapToHex(isVoid, VOID),
+        MapToHex(loop, LOOP),
+        MapToHex(logical, LOGICAL),
+        MapToHex(number, DATA),
+        MapToHex(op, OP),
+        MapToHex(output, OUTPUT),
+        MapToHex(subTypes, SUBTYPE),
+        MapToHex(whitespace, WHITESPACE)
+    };
+};
+
+struct Token {
+    token::Type type = token::Type::UNDEFINEDT;
+    token::SubType subType = token::SubType::UNDEFINEDST;
+    token::DataType dataType = token::DataType::UNDEFINEDDT;
+
+    std::string value = "";
+    std::string color;
+
+    int line;
+    int column;
+
+    Token() {}
+
+    Token(token::Type type, std::string value, std::string color, int line, int column) : 
+        type(type), 
+        value(value), 
+        color(color),
+        line(line), 
+        column(column) 
+        {}
+
+    Token(token::DataType dataType, std::string value, std::string color, int line, int column) : 
+        type(token::DATA),
+        dataType(dataType), 
+        value(value), 
+        color(color),
+        line(line), 
+        column(column) 
+        {}
+};
